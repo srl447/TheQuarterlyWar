c37ec758e3a4866f165605124ef49089a24635aa
Unknown <srl447@nyu.edu>
2016-11-24 12:13:02 -0500

    Improved Dust Particles
    Much closer to how dust should act

--- The Quarterly War/Assets/Scripts/Dust.cs
+++ The Quarterly War/Assets/Scripts/Dust.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Dust : MonoBehaviour
 {
     public ParticleSystem dusty;
+    Vector3 adjust = new Vector3(0, -1.8f, 0);
 	// Use this for initialization
 	void Start ()
     {
@@ -15,4 +16,20 @@ public class Dust : MonoBehaviour
     {
 	
 	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Floor" || coll.gameObject.tag == "Platform")
+        {
+            ParticleSystem newDust = Instantiate(dusty) as ParticleSystem;
+            newDust.transform.position = transform.position + adjust;
+        }
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor" || col.gameObject.tag == "Platform")
+        {
+            ParticleSystem newDust = Instantiate(dusty) as ParticleSystem;
+            newDust.transform.position = transform.position + adjust;
+        }
+    }
 }
